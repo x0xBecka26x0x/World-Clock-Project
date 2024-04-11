d70bc2927f1fe79ac006d4dd446b973cadc6d2c8
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-04-10 21:00:40 -0400

    update CSS & start of JS

---
+++ Javascript/java.js
@@ -0,0 +1,12 @@
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+let losAngelesTime = moment();
+
+
+losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]a[</small>]");
+
+
+
+
