a9e90495c37a9902a88dabab3d152fa6ee8f8c80
x0xBecka26x0x <rebecca.thombs@aol.com>
2024-04-13 19:12:36 -0400

    Added the functionality of the Select

--- Javascript/java.js
+++ Javascript/java.js
@@ -1,5 +1,5 @@
-setInterval(function() {
-
+function updateTime() {
+// Los Angeles
 let losAngelesElement = document.querySelector("#los-angeles");
 let losAngelesDateElement = losAngelesElement.querySelector(".date");
 let losAngelesTimeElement = losAngelesElement.querySelector(".time");
@@ -7,11 +7,11 @@ let losAngelesTime = moment().tz("America/Los_Angeles");
 
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]a[</small>]");
-}, 1000);
+losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]a[</small>]"
+);
 
 
-setInterval(function() {
+// New York
 let newYorkElement = document.querySelector("#new-york");
 let newYorkDateElement = newYorkElement.querySelector(".date");
 let newYorkTimeElement = newYorkElement.querySelector(".time");
@@ -19,9 +19,31 @@ let newYorkTime = moment().tz("America/New_York");
 
 
 newYorkDateElement.innerHTML = newYorkTime.format("MMMM Do YYYY");
-newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]a[</small>]");
-}, 1000);
+newYorkTimeElement.innerHTML = newYorkTime.format("h:mm:ss [<small>]a[</small>]"
+);
+}
 
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = `
+    <div class="city">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} 
+    <small>${cityTime.format("a")}</small></div>
+    </div>
+    `;
 
+}
 
+updateTime();
+setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#cities");
+citySelectElement.addEventListener("change", updateCity);
 
